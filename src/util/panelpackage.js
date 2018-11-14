c1cf1ad58be3474762d7b8f0b2d614fab46aead4
一剑光寒十四州 <fenghuijiuzhuan@163.com>
2018-11-14 15:56:21 +0800

    完善细节

--- src/util/panelpackage.js
+++ src/util/panelpackage.js
@@ -7,12 +7,19 @@ define([], function() {
     'no': function(){
       return '<div class="text-center"><p style="font-size:22px;margin-bottom: 30px;color: #FF5722;">不达标</p><p><i class="layui-icon layui-icon-face-cry" style="font-size: 50px; color: #FF5722;"></i></div>'
     },
+    'num': function (n) {
+      if(n<0){
+        return  '<div class="text-center" style="font-size: 20px;"><span class="fweight" style="color: #fb3f00;font-size: 36px;">'+n+'</span>分</div>'
+      }
+      return  '<div class="text-center" style="font-size: 20px;"><span class="fweight" style="color: #4ac444;font-size: 36px;">'+n+'</span>分</div>'
+    },
     'txt': function () {
       return ;
     }
   }
   function panelPackage(mod) {
-    return model[mod]()
+    mod = mod.split(':');
+    return model[mod[0]](mod[1])
   }
   return panelPackage
 });
